b9d20c25c4608df8d4f81bef5ccde9e56b539b9d
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-01-29 08:15:54 +0530

    feat: add patient initialization message

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hospital Management System - Trunk Based");
+        System.out.println("Patient module initialized");
+
     }
 }
